8d43e10e06963bae8638d0fe1380836a9f40d8bd
taro <teroro1226@outlook.jp>
2023-05-24 08:16:14 +0900

    feat: translated kanji to hiragana functions

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ import bs4
 import re
 import datetime
 import csv
+import pykakasi
 
 HTML_PATH = "../public_html/member_graduate.html"
 CSV_PATH = "../doc/doc_obog/"
@@ -14,6 +15,7 @@ soup = bs4.BeautifulSoup(html_text, "html.parser")
 
 name_list = list()
 reseach_list = list()
+hiragana_list = list()
 
 # divのclass名がmemb_textのものをスクレイピングしてくる
 people = soup.find_all("div", {"class", "memb_text"})
@@ -27,18 +29,26 @@ for i in range(length):
     if not profile:
         break
 
-    name, reseach = profile[0], profile[1]
+    name, reseach = str(profile[0]), str(profile[1])
     
-    name_list.append(str(name))
-    reseach_list.append(str(reseach))
+    name_list.append(name)
+    reseach_list.append(reseach)
+
+    # 形態素解析処理を実装する。
+    # 翻訳機を生成し、hiragana_listに格納する。
+    translater = pykakasi.kakasi()
+    result = translater.convert(name)
+    # resultがどのようなデータ構造なのかを確認する。
+    # print(result[0]["hira"])
+    hiragana_list.append(result[0]["hira"])
 
 # csv化する
 # columnのヘッダーを作成
-column = ["氏名", "研究内容"]
+column = ["氏名", "研究内容", "ひらがな"]
 with open(CSV_PATH + CSV_NAME, "w") as f:
     writer = csv.writer(f)
     # ヘッダーを挿入
     writer.writerow(column)
     # １行ずつインサートしていく
     for i in range(length - 1):
-        writer.writerow([name_list[i], reseach_list[i]])
+        writer.writerow([name_list[i], reseach_list[i], hiragana_list[i]])
